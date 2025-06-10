6e38df8d5eae2c36b95bad29827cbeeca763efc2
BLOKEPICK <Blokeplaycom@gmail.com>
2025-06-10 18:51:20 -0400

    NEW UPDATE

--- pages/api/discord-login.js
+++ pages/api/discord-login.js
@@ -2,7 +2,7 @@
 import { v4 as uuidv4 } from 'uuid';
 
 export default async function handler(req, res) {
-  const state = uuidv4(); // Opcional: podrías almacenarlo para validar después
+  const state = uuidv4();
   const redirect_uri = encodeURIComponent(process.env.DISCORD_REDIRECT_URI);
 
   const url = `https://discord.com/api/oauth2/authorize?client_id=${process.env.DISCORD_CLIENT_ID}` +
