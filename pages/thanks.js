145cb021f21341366bff2a753780179f678d80aa
BLOKEPICK <Blokeplaycom@gmail.com>
2025-06-10 19:12:42 -0400

    Discord

---
+++ pages/thanks.js
@@ -0,0 +1,9 @@
+
+export default function Thanks() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '100px', color: 'white' }}>
+      <h1>You're successfully connected with Discord!</h1>
+      <p>You can now continue with your subscription.</p>
+    </div>
+  );
+}
