aa7b7455b5c739b44dd3778a07bb5d83d1a5466c
BLOKEPICK <Blokeplaycom@gmail.com>
2025-06-12 03:39:38 -0400

    Form

---
+++ pages/VIPForm.jsx
@@ -0,0 +1,9 @@
+
+export default function VIPFormPage() {
+  return (
+    <div style={{ padding: '2rem' }}>
+      <h1>Formulario VIP</h1>
+      <p>Esta es la página protegida donde pronto mostraremos el formulario.</p>
+    </div>
+  );
+}
