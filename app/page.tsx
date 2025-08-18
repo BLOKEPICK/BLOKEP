d21772b16b4db9b2abd6be5fbcd49c55aefe6565
BLOKEPICK <Blokeplaycom@gmail.com>
2025-08-17 23:33:46 -0400

    up

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,19 @@
 // Home composition
 "use client";
 import React from "react";
-import Header from "@/components/Header";
-import Hero from "@/components/Hero";
-import TrustBar from "@/components/TrustBar";
-import Services from "@/components/Services";
-import Projects from "@/components/Projects";
-import Reviews from "@/components/Reviews";
-import Process from "@/components/Process";
-import Areas from "@/components/Areas";
-import FAQ from "@/components/FAQ";
-import Contact from "@/components/Contact";
-import Footer from "@/components/Footer";
-import StickyCTA from "@/components/StickyCTA";
-import { COMPANY, AREAS } from "@/components/constants";
+import Header from "../components/Header";
+import Hero from "../components/Hero";
+import TrustBar from "../components/TrustBar";
+import Services from "../components/Services";
+import Projects from "../components/Projects";
+import Reviews from "../components/Reviews";
+import Process from "../components/Process";
+import Areas from "../components/Areas";
+import FAQ from "../components/FAQ";
+import Contact from "../components/Contact";
+import Footer from "../components/Footer";
+import StickyCTA from "../components/StickyCTA";
+import { COMPANY, AREAS } from "../components/constants";
 
 export default function HomePage() {
   return (
